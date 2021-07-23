33889325f8d8fdfe5b23cce7ebb779ae850538f1
Terry Light <tlight2012@fau.edu>
2021-07-23 18:33:48 -0400

    learn how to create a new file with try/catch

--- InputOutput/getFileList.java
+++ InputOutput/getFileList.java
@@ -1,1 +1,13 @@
+import java.io.File;
+import java.util.Date;
 
+public class Exercise1 {
+     public static void main(String a[])
+     {
+        File file = new File("/home/revature/javaexercise/inputoutput");
+        String[] fileList = file.list();
+        for(String name:fileList){
+            System.out.println(name);
+        }
+    }
+}
